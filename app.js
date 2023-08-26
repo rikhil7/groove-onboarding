a25a81114d604c202d74678b339467d736b0e513
rikhil7 <rikhil.taneja@kalvium.community>
2023-08-26 10:59:46 +0530

    final

---
+++ app.js
@@ -0,0 +1,6 @@
+var buyBtn = document.getElementById("buy-now")
+var text = document.getElementById("thanks-text")
+buyBtn.onclick=()=>{
+    text.style.display="block"
+    buyBtn.style.display="none"
+}
